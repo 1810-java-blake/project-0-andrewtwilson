1e93d5f0efa1bd006a7eac3985d186214a089944
Andrew <wilson.andrew008@gmail.com>
2018-10-23 21:43:47 -0500

    Add comments for readability and add code to direct to gamepage

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     let canvas = document.getElementById("canvas");
     let pictureBtn = document.getElementById("pictureBtn");
     let emotionBtn = document.getElementById("emotionBtn");
+    let gamePageBtn = document.getElementById("gamePageBtn");
 
+    // Look for media devices that have video capabilities and
+    // add the stream to the video element if it is found.
     navigator.mediaDevices.getUserMedia({
         audio: false,
         video: true
@@ -17,18 +20,14 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log(`The error is: ${err}`);
         });
 
+    // On clicking the pictureBtn, draw the image from the stream
+    // to the canvas.
     pictureBtn.addEventListener("click", event => {
         var ctx = canvas.getContext("2d");
         ctx.drawImage(video, 0, 0);
     })
 
-    // var myHeaders = new Headers();
-    // myHeaders.append('Ocp-Apim-Subscription-Key', 'a5d4b501a2d04f2d946a0a2dc164c176');
-
-    // var myInit = {
-    //     headers: myHeaders,
-    // };
-
+    // Attempt at fetching from the Azure face api
     emotionBtn.addEventListener("click", event => {
         fetch("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect",{
             method:'POST',
@@ -40,5 +39,9 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log(data);
         })
         .catch(err => console.log(err));
+    })
+
+    gamePageBtn.addEventListener("click", event => {
+        window.location = "./gamepage.html";
     })
 })
