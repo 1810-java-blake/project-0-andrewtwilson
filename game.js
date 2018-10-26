712e10d9f1cb051d364fade3e2778079fcfb0ae3
Andrew <wilson.andrew008@gmail.com>
2018-10-26 13:53:12 -0500

    Add generality to the ball physics; can add more balls easily

--- game.js
+++ game.js
@@ -6,113 +6,163 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let sketchProc = function (processing) {
 
-        function pythag(x1, y1, x2, y2) {
-            return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        function euclidDistance(x1, y1, x2, y2) {
+            // console.log(x1 + " " + y1 + " " + x2 + " " + y2 + " ")
+            return (Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)));
         }
 
-        // keyword "with" puts "processing" object on top of the scope chain
-        with (processing) {
-
-            // declare the Ball class, with properties for the physics calculations
-            class Ball {
-                constructor(xp, yp, radius, mass, mag, dir) {
-                    this.xp = xp;
-                    this.yp = yp;
-                    this.radius = radius;
-                    this.mass = mass;
-                    this.vector = {
-                        magnitude: mag,
-                        direction: dir
-                    };
-                    this.vx = 0;
-                    this.vy = 0;
-                    this.newxv = 0;
-                    this.newxy = 0;
-                }
-                calcMagnitude() {
-                    this.vector.magnitude = Math.sqrt(this.vx * this.vx + this.vy * this.vy)
-                }
-                // splitVectorXY requires a vector magnitude and direction,
-                // and splits this into its x and y components.
-                // Currently only used after initialization
-                splitVectorXY() {
-                    this.vx = this.vector.magnitude * Math.cos(this.vector.direction);
-                    this.vy = this.vector.magnitude * Math.sin(this.vector.direction);
-                }
-                // calcAngle() calculates the angle that the ball is 
-                // traveling given the vector's x and y components
-                calcAngle() {
-                    this.vector.direction = Math.atan2(this.vy, this.vx);
-                }
-                // update() function updates the balls position, moving
-                // it every call of the Draw() function according to it's velocity,
-                // then draws the ball again.
-                update() {
-                    this.xp += this.vx;
-                    this.yp += this.vy;
-                    ellipse(this.xp, this.yp, this.radius * 2, this.radius * 2);
-                }
+        // declare the Ball class, with properties for the physics calculations
+        class Ball {
+            constructor(xp, yp, radius, mass, mag, dir, r, g, b) {
+                this.xp = xp;
+                this.yp = yp;
+                this.radius = radius;
+                this.mass = mass;
+                this.vector = {
+                    magnitude: mag,
+                    direction: dir
+                };
+                this.color = {
+                    red: r,
+                    green: g,
+                    blue: b
+                };
+                this.vx = 0;
+                this.vy = 0;
+                this.newxv = 0;
+                this.newxy = 0;
+                this.hasCollided = false;
             }
+            // calcMagnitude() {
+            //     this.magnitude = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+            // }
+            // // splitVectorXY requires a vector magnitude and direction,
+            // // and splits this into its x and y components.
+            // // Currently only used after initialization
+            // splitVectorXY() {
+            //     this.vx = this.magnitude * Math.cos(this.direction);
+            //     this.vy = this.magnitude * Math.sin(this.direction);
+            // }
+            // // calcAngle() calculates the angle that the ball is 
+            // // traveling given the vector's x and y components
+            // calcAngle() {
+            //     this.direction = Math.atan2(this.vy, this.vx);
+            // }
+            // // update() function updates the balls position, moving
+            // // it every call of the Draw() function according to it's velocity,
+            // // then draws the ball again.
+            // update() {
+            //     this.xp += this.vx;
+            //     this.yp += this.vy;
+            //     this.hasCollided = false;
+            //     this.distToEachBall = [];
+            //     processing.ellipse(this.xp, this.yp, this.radius * 2, this.radius * 2);
+            //     processing.fill(this.color.r, this.color.g, this.color.b);
+            // }
+        }
+        function calcMagnitude(balls) {
+            balls.forEach(ball => {
+                ball.vector.magnitude = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
+            })
+        }
+        // splitVectorXY requires a vector magnitude and direction,
+        // and splits this into its x and y components.
+        // Currently only used after initialization
+        function splitVectorXY(balls) {
+            balls.forEach(ball => {
+                ball.vx = ball.vector.magnitude * Math.cos(ball.vector.direction);
+                ball.vy = ball.vector.magnitude * Math.sin(ball.vector.direction);            
+            })
+        }
+        // calcAngle() calculates the angle that the ball is 
+        // traveling given the vector's x and y components
+        function calcAngle(balls) {
+            balls.forEach(ball => {
+                let temp = Math.atan2(ball.vy, ball.vx);
+                if (temp <= 0) {
+                    temp += 2*Math.PI;
+                }
+                // console.log(temp);
+                ball.vector.direction = temp;
+            })
+        }
+        // update() function updates the balls position, moving
+        // it every call of the Draw() function according to it's velocity,
+        // then draws the ball again.
+        function update(balls) {
+            balls.forEach(ball => {
+                ball.xp += ball.vx;
+                ball.yp += ball.vy;
+                ball.hasCollided = false;
+                processing.fill(ball.color.red, ball.color.green, ball.color.blue);
+                processing.ellipse(ball.xp, ball.yp, ball.radius * 2, ball.radius * 2);
+            })
+        }
 
-            let balls = [];
+        let balls = [];
 
-            balls.push(new Ball(50, 190, 25, 25, 1, 0));
-            balls.push(new Ball(350, 200, 25, 500, 1, Math.PI));
+        balls.push(new Ball(50, 190, 25, 2, 0.1, 0, 255, 0, 0));
+        balls.push(new Ball(350, 200, 25, 10, 0.1, Math.PI, 1, 1, 1,));
+        balls.push(new Ball(110, 200, 25, 3, 0, 0, 0, 0, 255));
+        balls.push(new Ball(110, 300, 15, 3, 0, 0, 0, 255, 0));
 
-            balls.foreach( element => element.splitVectorXY );
+        splitVectorXY(balls);
 
-            //let ball = new Ball(50, 190, 25, 25, 1, 0);
-            //let ball2 = new Ball(350, 200, 25, 500, 1, Math.PI);
+        processing.frameRate(400);
+        processing.size(400, 400);
+        processing.fill(0, 0, 0);
 
-            // ball.splitVectorXY();
-            // ball2.splitVectorXY();
+        processing.draw = function () {
+            processing.background(199, 185, 185);
+            // Update the balls' positions after the background is redrawn.
+            update(balls);
+            let distToEachBall = [];
+            let angleBetweenEachBall = [];
 
-            frameRate(30);
-            size(400, 400);
+            balls.forEach(() => {
+                distToEachBall.push([]);
+                angleBetweenEachBall.push([]);
+            })
 
-            fill(0, 0, 0);
+            balls.forEach((ball, index) => {
+                for (i = 0; i < balls.length; i++) {
+                    if (i !== index) {
+                        distToEachBall[index][i] = euclidDistance(ball.xp, ball.yp, balls[i].xp, balls[i].yp);
+                        angleBetweenEachBall[index][i] = Math.atan2((ball.yp - balls[i].yp), (ball.xp - balls[i].xp));
+                    }
+                }
+            })
+            
+            // Check if the distance between the balls' centers is smaller
+            // than their combined radii.
+            balls.forEach((ball, index) => {
+                for (i = 0; i < balls.length; i++) {
+                    if (distToEachBall[index][i] <= (ball.radius + balls[i].radius) && i !== index) {
+                        collision.innerHTML = "YES";
 
-            draw = function () {
-                background(199, 185, 185);
-                // Update the balls' positions after the background is redrawn.
-                fill(255, 0, 0);
-                ball.update();
-                fill(0, 0, 0);
-                ball2.update();
+                        ball.hasCollided = true;
 
-                // Phi is the angle between the two balls
-                let phi = Math.asin(Math.abs(ball2.yp - ball.yp) / (ball.radius + ball2.radius));
-                let dotProduct = ball.vector.magnitude * ball2.vector.magnitude * cos(phi);
+                        ball.newvx = (ball.vector.magnitude * Math.cos(ball.vector.direction - angleBetweenEachBall[index][i]) * (ball.mass - balls[i].mass) + 2 * balls[i].mass * balls[i].vector.magnitude * Math.cos(balls[i].vector.direction - angleBetweenEachBall[index][i]))
+                            / (ball.mass + balls[i].mass) * Math.cos(angleBetweenEachBall[index][i]) + ball.vector.magnitude * Math.sin(ball.vector.direction - angleBetweenEachBall[index][i]) * Math.sin(angleBetweenEachBall[index][i]);
 
-                // Check if the distance between the balls' centers is smaller
-                // than their combined radii.
-                if (pythag(ball.xp, ball.yp, ball2.xp, ball2.yp) <= (ball.radius + ball2.radius) && dotProduct >= 0) {
-                    collision.innerHTML = "Yes";
-
-                    // Calculate the new velocities of the balls using PHYSICS.
-                    ball.newvx = (ball.vector.magnitude * Math.cos(ball.vector.direction - phi) * (ball.mass - ball2.mass) + 2 * ball2.mass * ball2.vector.magnitude * Math.cos(ball2.vector.direction - phi)) / (ball.mass + ball2.mass) * Math.cos(phi) + ball.vector.magnitude * Math.sin(ball.vector.direction - phi) * Math.sin(phi);
-                    ball.newvy = (ball.vector.magnitude * Math.cos(ball.vector.direction - phi) * (ball.mass - ball2.mass) + 2 * ball2.mass * ball2.vector.magnitude * Math.cos(ball2.vector.direction - phi)) / (ball.mass + ball2.mass) * Math.sin(phi) + ball.vector.magnitude * Math.sin(ball.vector.direction - phi) * Math.cos(phi);
-                    ball2.newvx = (ball2.vector.magnitude * Math.cos(ball2.vector.direction - phi) * (ball2.mass - ball.mass) + 2 * ball.mass * ball.vector.magnitude * Math.cos(ball.vector.direction - phi)) / (ball2.mass + ball.mass) * Math.cos(phi) + ball2.vector.magnitude * Math.sin(ball2.vector.direction - phi) * Math.sin(phi);
-                    ball2.newvy = (ball2.vector.magnitude * Math.cos(ball2.vector.direction - phi) * (ball2.mass - ball.mass) + 2 * ball.mass * ball.vector.magnitude * Math.cos(ball.vector.direction - phi)) / (ball2.mass + ball.mass) * Math.sin(phi) + ball2.vector.magnitude * Math.sin(ball2.vector.direction - phi) * Math.cos(phi);
-
-                    // Give each ball its new velocity.
+                        ball.newvy = (ball.vector.magnitude * Math.cos(ball.vector.direction - angleBetweenEachBall[index][i]) * (ball.mass - balls[i].mass) + 2 * balls[i].mass * balls[i].vector.magnitude * Math.cos(balls[i].vector.direction - angleBetweenEachBall[index][i])) 
+                            / (ball.mass + balls[i].mass) * Math.sin(angleBetweenEachBall[index][i]) + ball.vector.magnitude * Math.sin(ball.vector.direction - angleBetweenEachBall[index][i]) * Math.cos(angleBetweenEachBall[index][i]);
+                    }
+                }
+            })
+            balls.forEach(ball => {
+                if (ball.hasCollided === true) {
                     ball.vx = ball.newvx;
                     ball.vy = ball.newvy;
-                    ball2.vx = ball2.newvx;
-                    ball2.vy = ball2.newvy;
 
                     ball.xp = ball.xp + ball.vx;
                     ball.yp = ball.yp + ball.vy;
-                    ball2.xp = ball2.xp + ball2.vx;
-                    ball2.yp = ball2.yp + ball2.vy;
-                    ellipse(this.xp, this.yp, this.radius * 2, this.radius * 2);
+                }
+            })
 
-                    //ball.calcAngle();
-                    //ball2.calcAngle();
-                };
-
-                // Check if the balls are touching or inside the wall.
-                // If inside the wall, move the ball just outside
+            // Check if the balls are touching or inside the wall.
+            // If inside the wall, move the ball just outside
+            balls.forEach(ball => {
                 if (ball.xp <= ball.radius) {
                     ball.xp = ball.radius;
                     ball.vx *= -1;
@@ -129,31 +179,10 @@ document.addEventListener("DOMContentLoaded", () => {
                     ball.yp = 400 - ball.radius;
                     ball.vy *= -1;
                 }
-                if (ball2.xp <= ball2.radius) {
-                    ball2.xp = ball2.radius;
-                    ball2.vx *= -1;
-                }
-                else if (ball2.xp >= (400 - ball2.radius)) {
-                    ball2.xp = 400 - ball2.radius;
-                    ball2.vx *= -1;
-                }
-                else if (ball2.yp <= ball2.radius) {
-                    ball2.yp = ball2.radius;
-                    ball2.vy *= -1;
-                }
-                else if (ball2.yp >= (400 - ball2.radius)) {
-                    ball2.yp = 400 - ball2.radius;
-                    ball2.vy *= -1;
-                }
-                // // Check if the balls are touching/inside each other
-                // if (pythag(ball.xp, ball.yp, ball2.xp, ball2.yp) <= (ball.radius + ball2.radius)) {
+            })
 
-                // }
-                ball.calcMagnitude();
-                ball2.calcMagnitude();
-                ball.calcAngle();
-                ball2.calcAngle();
-            }
+            calcMagnitude(balls);
+            calcAngle(balls);
         }
     };
 
